73eeb42d833b93789cc80dfa17d2930c618f345c
decladrian <adrian@declarando.es>
2022-03-05 16:39:42 +0100

    1 file modified, 1 deleted

--- index.tsx
+++ index.tsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import './style.css';
 import { Post } from './features/post/ui/Post/Post';
 import { PostForm } from './features/post/ui/PostForm/PostForm';
 import { CreateGroup } from './features/group/ui/CreateGroup/CreateGroup';
