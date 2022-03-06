135a89cf721225e47be195e5054e97532e35c0e2
decladrian <adrian@declarando.es>
2022-03-06 19:16:14 +0100

    1 file modified

--- shared/application/Controller.ts
+++ shared/application/Controller.ts
@@ -1,7 +1,5 @@
 import { container, libs } from '../../container';
 
 export class Controller {
-  protected readonly logger = libs.Logger;
-  protected readonly query = container.Query;
-  protected readonly command = container.Command;
+
 }
