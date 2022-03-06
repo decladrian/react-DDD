6c3f6a5202e0d07c786ce6c85f3d0dd5e72a8dd6
decladrian <adrian@declarando.es>
2022-03-06 11:42:12 +0100

    2 files modified

--- features/post/ui/PostForm/usePostFormReducer.ts
+++ features/post/ui/PostForm/usePostFormReducer.ts
@@ -1,5 +1,4 @@
 import { useReducer } from 'react';
-//import { PostEntity } from '../../domain/PostEntity';
 import { PostModels } from '../../domain/PostModels';
 
 export const usePostFormReducer = () => {
--- shared/domain/index.ts
+++ shared/domain/index.ts
@@ -1,6 +1,5 @@
 export { MyApi } from './MyApi';
 export { SemanticTypes } from './SemanticTypes';
 export { Validation } from './Validation';
-export { Entity } from './Entity';
 export { ValidationError } from './error/ValidationError';
 export { ErrorTypes } from './error/ErrorTypes';
